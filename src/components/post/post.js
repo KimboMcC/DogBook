9332bd2969c7dd02ef3c3dbc9fe01fd64a46b275
KimboMcC <kimmccauleybn7@gmail.com>
2023-10-23 13:21:04 +0100

    Initlal commit

---
+++ src/components/post/post.js
@@ -0,0 +1,21 @@
+
+const Post = () => {
+    return (
+        <>
+            <div className="whole-post">
+                <div className="flex space-x-9 items-center">
+                    <div className="bg-black w-10 h-10"></div>
+                    <h3 className="">Kim</h3>
+                </div>
+                <div className="post-content">
+
+                </div>
+                <div className="post-bottom">
+                
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Post;
